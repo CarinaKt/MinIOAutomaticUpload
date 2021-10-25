aab3142faf293766d5272ada609793882793a32f
CarinaKt <carina.kuehnert@web.de>
2021-10-25 15:05:35 +0200

    Add directory name via command line

--- src/main/java/FileWatcher.java
+++ src/main/java/FileWatcher.java
@@ -1,20 +1,37 @@
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Scanner;
+
 import static java.nio.file.StandardWatchEventKinds.*;
 
 public class FileWatcher {
 
-    //the directory to watch on C:/Users/Carina/Documents/
-    final Path path = FileSystems.getDefault().getPath(System.getProperty("user.home"), "Documents");
+    /**
+     * Reads in the Directory Name from the command line
+     * TODO: Validation
+     * @return
+     */
+    private String getDirectory() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a directory: ");
+        //reads directory nme
+        return sc.nextLine();
+    };
+
+    // Directory to trac
+    final String directory = getDirectory();
+    // the directory path to watch on
+    final Path path = FileSystems.getDefault().getPath(System.getProperty("user.home"), directory);
 
     public void watcher() {
         try (final WatchService watchService = FileSystems.getDefault().newWatchService()) {
             // listen for events
             final WatchKey watchKey = path.register(
                     watchService,
-                   // ENTRY_MODIFY, not in use at the moment
-                    ENTRY_CREATE // new an rename
+                    ENTRY_CREATE // new or rename
             );
+
+            System.out.println("Watching "+directory+" for changes");
 
             while (true) {
                 final WatchKey wk = watchService.take();
@@ -22,6 +39,8 @@ public class FileWatcher {
                     //the context is always a Path.
                     final Path changed = (Path) event.context();
                     System.out.println(changed);
+                    // send path to MInIO API
+                    // TODO
                 }
                 // reset the key
                 boolean valid = wk.reset();
