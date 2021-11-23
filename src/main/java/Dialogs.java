f42138920cb576ea38a554dbcf9ae2f9111a208b
CarinaKt <carina.kuehnert@web.de>
2021-11-23 21:15:34 +0100

    Add Dialog class

---
+++ src/main/java/Dialogs.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Dialogs{
+    public static void main(String[] bla){
+
+        // Aufruf der statischen Methode showConfirmDialog()
+        int eingabe = JOptionPane.showConfirmDialog(null,
+                "Geben Sie Ihr Einverständnis?",
+                "Einverständnis",
+                JOptionPane.YES_NO_CANCEL_OPTION);
+        System.out.println(eingabe);
+        System.exit(0);
+
+    }
+}
