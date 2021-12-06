fdb20a34195498b44f75cc25389d74a5c1062cee
CarinaKt <carina.kuehnert@web.de>
2021-12-06 20:48:23 +0100

    Add Input Dialog, add Logging

--- src/main/java/DialogWithRadiobutton.java
+++ src/main/java/DialogWithRadiobutton.java
@@ -1,6 +1,12 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.logging.Logger;
 
 public class DialogWithRadiobutton extends JDialog {
     private JPanel DialogWithButton;
@@ -8,16 +14,20 @@ public class DialogWithRadiobutton extends JDialog {
     private JButton buttonCancel;
     private JRadioButton removeFromLocalRadioButton;
     private JRadioButton keepLocalTooRadioButton;
-    private JTextField cUserCarinaTextField;
+    private JTextField sourceFolderPathTextField;
     private JRadioButton nothingRadioButton;
     private JRadioButton uploadToTheCloudRadioButton;
     private JPanel sourceFolder;
     private JPanel Button;
     private JPanel AdditionalOptions;
 
+    // Logging
+    Logger logger =  Logger.getLogger(this.getClass().getName());
+    Path path = FileSystems.getDefault().getPath(System.getProperty("user.home"),"\\AppData\\MinIO");
+
     public DialogWithRadiobutton() {
         setContentPane(DialogWithButton);
-        setTitle("Edit options");
+        setTitle("Edit Options");
         getRootPane().setDefaultButton(buttonOK);
         //setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         //setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -52,25 +62,132 @@ public class DialogWithRadiobutton extends JDialog {
     }
 
     private void onOK() {
-        // add your code here
+        // prof the path
+        String directory = sourceFolderPathTextField.getText();
+        if(Files.isDirectory(Path.of(directory))){
+           logger.info("Directory exists!");
+
+            try {
+
+                if (!Files.exists(path)) {
+                    Files.createDirectory(path);
+                    System.out.println("Directory created");
+                    logger.info("Directory created" + path);
+                }
+
+                File myObj = new File( path.toString()+"\\uploadConfig.ini");
+                if (myObj.createNewFile()) {
+                    logger.info("File created: " + myObj.getName());
+
+                    FileWriter myWriter = new FileWriter(myObj);
+                    directory = directory.replace("\\", "\\\\");
+                    System.out.println(directory);
+                    myWriter.write("path=" + directory + "\n");
+                    myWriter.write("removeExportedData=" + removeFromLocalRadioButton.isSelected() + "\n");
+                    myWriter.write("uploadExistingData=" + uploadToTheCloudRadioButton.isSelected() + "\n");
+                    myWriter.close();
+
+                } else {
+                    logger.info("File already exists.");
+                }
+            } catch (IOException e) {
+                logger.warning("An error occurred." + e);
+                e.printStackTrace();
+            }
+
+        }else {
+            logger.warning("no existing folder");
+            // TODO: Ask to create the Folder/ change the entry
+        }
+
         dispose();
     }
 
     private void onCancel() {
         // add your code here if necessary
         dispose();
+        logger.info("Close Dialog");
     }
 
-    public static void main(String[] args) {
-        DialogWithRadiobutton myDialog = new DialogWithRadiobutton();
-        myDialog.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        myDialog.pack();
-        myDialog.setVisible(true);
-        //System.exit(0);
+    public static void loadDialog() {
+        // Only Open if ini.txt doesnt exists
+        Path path = FileSystems.getDefault().getPath(System.getProperty("user.home"),"\\AppData\\MinIO\\uploadConfig.ini");
+        if (!Files.exists(path)) {
+            DialogWithRadiobutton myDialog = new DialogWithRadiobutton();
+            myDialog.createUIComponents();
+            myDialog.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+            myDialog.pack();
+            myDialog.setVisible(true);
+        } else {
+            Logger.getAnonymousLogger().info("File already exists: " + path);
+            System.exit(0);
+        }
     }
 
     private void createUIComponents() {
-        // TODO: place custom component creation code here
+        // set default path as input
+        sourceFolderPathTextField.setText(FileSystems.getDefault().getPath(System.getProperty("user.home"),
+                "\\FolderToTrack").toString());
+
+        // Radiobutton "exported Data"
+        removeFromLocalRadioButton.setSelected(true);
+        keepLocalTooRadioButton.setSelected(false);
+
+        // Radiobutton "existing Data"
+        nothingRadioButton.setSelected(true);
+        uploadToTheCloudRadioButton.setSelected(false);
+
+        // Listener make sure that only one Radiobutton can be selected
+        removeFromLocalRadioButton.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 if(removeFromLocalRadioButton.isSelected()){
+                     keepLocalTooRadioButton.setSelected(false);
+                 }else{
+                     keepLocalTooRadioButton.setSelected(true);
+                 }
+                 logger.info("removeFromLocalRadioButton.isSelected(): " + removeFromLocalRadioButton.isSelected()
+                         + "\n" + "keepLocalTooRadioButton: " + keepLocalTooRadioButton.isSelected());
+             }
+         });
+        keepLocalTooRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(keepLocalTooRadioButton.isSelected()){
+                    removeFromLocalRadioButton.setSelected(false);
+                }else{
+                    removeFromLocalRadioButton.setSelected(true);
+                }
+                logger.info("keepLocalTooRadioButton.isSelected(): " + keepLocalTooRadioButton.isSelected()
+                        + "\n" + "removeFromLocalRadioButton: " + removeFromLocalRadioButton.isSelected());
+            }
+        });
+
+        uploadToTheCloudRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(uploadToTheCloudRadioButton.isSelected()){
+                    nothingRadioButton.setSelected(false);
+                }else{
+                    nothingRadioButton.setSelected(true);
+                }
+                logger.info("uploadToTheCloudRadioButton.isSelected(): " + uploadToTheCloudRadioButton.isSelected()
+                        + "\n" + "nothingRadioButton: " + nothingRadioButton.isSelected());
+            }
+        });
+        nothingRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (nothingRadioButton.isSelected()) {
+                    uploadToTheCloudRadioButton.setSelected(false);
+                } else {
+                    uploadToTheCloudRadioButton.setSelected(true);
+                }
+                logger.info("nothingRadioButton.isSelected(): " + nothingRadioButton.isSelected()
+                        + "\n" + "uploadToTheCloudRadioButton: " + uploadToTheCloudRadioButton.isSelected());
+            }
+        });
+
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,13 @@ import io.minio.MinioClient;
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        // Input Dialog
+        DialogWithRadiobutton.loadDialog();
         // Connection to your MinIo Server
         MinioClient minioClient = MinIO.connection();
         // watch for changes
-        //new FileWatcher().watcher();
         new WatchServiceRecursive().watcher();
+
 
     }
 }
